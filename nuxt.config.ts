235f71fd7c53b83075dc989974d5dea4d5e24edc
Lucas TRAN <lucastran00@gmail.com>
2023-04-12 16:27:03 +0200

    src

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    srcDir: 'src/',
+
+    ssr: true,
     css: [
         '@/styles/shared.sass',
         '@/styles/core.sass',
