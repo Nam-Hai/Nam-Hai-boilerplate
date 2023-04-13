3fae7658850f970b31bdc29191186090b7ebac3f
Lucas TRAN <lucastran00@gmail.com>
2023-04-13 15:36:44 +0200

    parallax init

---
+++ pages/parallax.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="wrapper">
+        <div ref="ref1"></div>
+        <div ref="ref2"></div>
+        <div ref="ref3"></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const ref1 = ref()
+const ref2 = ref()
+const ref3 = ref()
+
+</script>
+
+<style lang="scss">
+    
+</style>
