2da964db6f4ba27bc54510af78896971391b7f7e
Lucas TRAN <lucastran00@gmail.com>
2023-04-13 19:02:01 +0200

    lenis

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,14 @@
 <template>
-    <TheSmoothScroll>
-        <slot />
-    </TheSmoothScroll>
+    <slot />
 </template>
 
 <script setup lang="ts">
+// configure lenis in @/plugins/lenis.client.ts
+const { $lenis } = useNuxtApp()
+
+useRaf((e)=>{
+    $lenis.raf(e.elapsed)
+})
 </script>
 
 <style lang="scss" scoped>
---
+++ plugins/lenis.client.ts
@@ -0,0 +1,13 @@
+import Lenis from "@studio-freight/lenis"
+
+export default defineNuxtPlugin(nuxtApp =>{
+
+  return {
+    provide: {
+        lenis: new Lenis({
+            normalizeWheel: true
+        })
+    }
+  }
+
+})
