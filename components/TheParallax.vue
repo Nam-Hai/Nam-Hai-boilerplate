4d28c9bb1150aedb6321e08f567db448f98a1129
Lucas TRAN <lucastran00@gmail.com>
2023-04-13 16:21:55 +0200

    theParallax component init

---
+++ components/TheParallax.vue
@@ -0,0 +1,28 @@
+<template>
+    <slot ref="rootRef" />
+</template>
+
+<script setup lang="ts">
+import { N } from '~/helpers/namhai-utils';
+
+
+const rootRef = ref()
+type Props = {
+    amount: number
+}
+const props = withDefaults(defineProps<Props>(), {
+    amount: 1
+})
+
+useScrollProgression({
+    el: rootRef,
+    vStart: 100,
+    end: 0,
+    onProgress: (t)=>{
+      N.T(rootRef.value, 0, (1 - props.amount) * t * 100, 'vh')
+    }
+  })
+</script>
+
+<style scoped>
+</style>
--- pages/parallax.vue
+++ pages/parallax.vue
@@ -9,12 +9,55 @@
 </template>
 
 <script setup lang="ts">
+import { N } from '~/helpers/namhai-utils';
+
 const ref1 = ref()
 const ref2 = ref()
 const ref3 = ref()
 
+onMounted(()=>{
+})
+
+const paralax = (el:Ref<HTMLElement>, amount: number)=>{
+  useScrollProgression({
+    el: el,
+    vStart: 100,
+    end: 0,
+    onProgress: (t)=>{
+      N.T(el.value, 0, (1 - amount) * t * 100, 'vh')
+    }
+  })
+}
+paralax(ref1, 1.09)
+paralax(ref2, 1.1)
+paralax(ref3, 1.05)
+
 </script>
 
 <style lang="scss">
-    
+.wrapper {
+  display: flex;
+  column-gap: 1rem;
+  height: 800rem;
+  margin-top: 170rem;
+  
+  
+  div {
+    width: 15rem;
+    height: 15rem;
+
+    &:nth-child(4n){
+      background: red;
+    }
+    &:nth-child(4n + 1){
+      background: blue;
+    }
+    &:nth-child(4n + 2){
+      background: crimson;
+    }
+    &:nth-child(4n + 3){
+      background-color: green;
+    }
+  }
+}
 </style>
