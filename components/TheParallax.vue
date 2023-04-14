e08ccbc4b8916c2e6afaa2f04eb09ca5fffa1da5
Lucas TRAN <lucastran00@gmail.com>
2023-04-14 13:15:16 +0200

    parallax component

--- components/TheParallax.vue
+++ components/TheParallax.vue
@@ -1,5 +1,7 @@
 <template>
-    <slot ref="rootRef" />
+<div ref="rootRef" >
+    <slot/>
+</div>
 </template>
 
 <script setup lang="ts">
@@ -7,22 +9,27 @@ import { N } from '~/helpers/namhai-utils';
 
 
 const rootRef = ref()
+
 type Props = {
     amount: number
 }
+
 const props = withDefaults(defineProps<Props>(), {
     amount: 1
 })
+
 
 useScrollProgression({
     el: rootRef,
     vStart: 100,
     end: 0,
     onProgress: (t)=>{
-      N.T(rootRef.value, 0, (1 - props.amount) * t * 100, 'vh')
+    //   console.log(t)
+      N.T(rootRef.value, 0, (1 - props.amount) * (t - 0.5) * 100, 'vh')
     }
   })
 </script>
 
 <style scoped>
+
 </style>
