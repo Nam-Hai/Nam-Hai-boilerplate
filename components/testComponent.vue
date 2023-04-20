ff3e3b4d7a0fde291cf1a13f9106953c77221f28
Lucas TRAN <lucastran00@gmail.com>
2023-04-20 02:56:28 +0200

    onFlow

--- components/testComponent.vue
+++ components/testComponent.vue
@@ -1,13 +1,29 @@
 <template>
-    <div class="wrapper">
-        COMPONENT
-    </div>
+  <div ref="wrapperRef" class="wrapper">
+    COMPONENT
+  </div>
 </template>
 <script setup lang="ts">
-    const {$TL} = useNuxtApp()
+import { onFlow } from '~/util/FlowProvider';
+
+const { $TL } = useNuxtApp()
+const wrapperRef = ref()
+
+onFlow(() => {
+  let tl = new $TL
+  tl.from({
+    el: wrapperRef.value,
+    p: {
+      r: [0, 180]
+    },
+    d: 500,
+  }).play()
+})
+
 </script>
 <style lang="scss" scoped>
-    .wrapper{
-        background-color: crimson !important;;
-    }
+.wrapper {
+  background-color: crimson !important;
+  ;
+}
 </style>
