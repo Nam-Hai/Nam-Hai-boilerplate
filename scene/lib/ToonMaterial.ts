8d61e703b97a55b417573615415cd22f263e4bb9
Lucas TRAN <lucastran00@gmail.com>
2023-05-22 11:07:09 +0200

    toon material

--- scene/lib/ToonMaterial.ts
+++ scene/lib/ToonMaterial.ts
@@ -3,51 +3,51 @@ import { Program, Texture, Color } from 'ogl'
 import RampMap, { RampMapOptions } from './RampMap'
 
 type PaletteType = Array<{
-    x: number,
-    hex: string
+  x: number,
+  hex: string
 }>
 
 export default class ToonMaterial extends Program {
-    uLightPosition: globalThis.Ref<number[]>
-    ramp: RampMap
-    tMap: { value: any }
-    constructor(gl: any, options: Partial<{ palette: PaletteType, lightPosition: [number, number, number] }> = {}) {
-        const palette = options.palette || [{ x: 0, hex: '#000000' }]
-        const uLightPosition = ref(options.lightPosition || [15, 15, 15])
-        console.log(palette, uLightPosition.value);
-        const ramp = new RampMap(palette)
+  uLightPosition: globalThis.Ref<number[]>
+  ramp: RampMap
+  tMap: { value: any }
+  constructor(gl: any, options: Partial<{ palette: PaletteType, lightPosition: [number, number, number] }> = {}) {
+    const palette = options.palette || [{ x: 0, hex: '#000000' }]
+    const uLightPosition = ref(options.lightPosition || [15, 15, 15])
+    console.log(palette, uLightPosition.value);
+    const ramp = new RampMap(palette)
 
-        const texture = {
-            value: new Texture(gl, {
-                image: ramp.gradientData,
-                width: ramp.resolution,
-                height: 1,
-                flipY: false,
-                generateMipmaps: false,
-                minFilter: gl.NEAREST,
-                magFilter: gl.NEAREST
-            }),
-        };
-        texture.value.needsUpdate = true;
+    const texture = {
+      value: new Texture(gl, {
+        image: ramp.gradientData,
+        width: ramp.resolution,
+        height: 1,
+        flipY: false,
+        generateMipmaps: false,
+        minFilter: gl.NEAREST,
+        magFilter: gl.NEAREST
+      }),
+    };
+    texture.value.needsUpdate = true;
 
-        super(gl, {
-            fragment,
-            vertex,
-            uniforms: {
-                tMap: texture,
-                uLightPosition
-            }
-        })
+    super(gl, {
+      fragment,
+      vertex,
+      uniforms: {
+        tMap: texture,
+        uLightPosition
+      }
+    })
 
-        this.uLightPosition = uLightPosition
-        this.ramp = ramp
-        this.tMap = texture
-    }
+    this.uLightPosition = uLightPosition
+    this.ramp = ramp
+    this.tMap = texture
+  }
 
-    addColor({ x, hex }: RampMapOptions) {
-        this.ramp.add({ x, hex })
-        this.tMap.value.image = this.ramp.gradientData
-    }
+  addColor({ x, hex }: RampMapOptions) {
+    this.ramp.add({ x, hex })
+    this.tMap.value.image = this.ramp.gradientData
+  }
 
 }
 const vertex = /* glsl */`#version 300 es
@@ -94,7 +94,10 @@ void main() {
     float brightness = dot( worldNormal, lightVector );
     brightness = (brightness + 1.) /2.;
 
+    float depth = dot(worldNormal, normalize(vec3(0.,1.,.6)) );
+
     FragColor = texture(tMap, vec2(brightness, 0.5));
+    FragColor.rgb += vec3(depth)/ 5.;
     FragColor.a = 1.;
 }
 `
