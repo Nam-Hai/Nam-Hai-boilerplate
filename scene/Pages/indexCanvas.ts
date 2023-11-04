ec7e78c6b88d8b348ce3e981f6e2cf74a1645d66
Nam Hai <lucas.tran@imt-atlantique.net>
2023-11-04 15:19:16 +0100

    Revert "des choses"
    This reverts commit 70c23b39b42821261b049790ef27d71bb5611e21.

--- scene/Pages/indexCanvas.ts
+++ scene/Pages/indexCanvas.ts
@@ -2,7 +2,6 @@ import { RafR, rafEvent } from "~/plugins/core/raf"
 import { ROR, ResizeEvent } from "~/plugins/core/resize"
 import Callstack from "../utils/Callstack"
 import { CanvasPage } from "../utils/types";
-import { WelcomeGL } from "../Components/Welcome";
 
 //@ts-ignore
 import { Transform } from "ogl";
@@ -38,8 +37,10 @@ export class IndexCanvas implements CanvasPage {
     this.raf = useRafR(this.render)
     this.destroyStack.add(() => this.raf.kill())
 
-    const a = new WelcomeGL(this.gl)
-    a.mesh.setParent(this.scene)
+
+    canvasWatch(useCanvas().size, (newSize) => {
+      this.ro.trigger()
+    })
   }
   init() {
     this.raf.run()
