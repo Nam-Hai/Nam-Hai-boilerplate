f962796342ebb80b46da7d1e14797726e449ce49
Nam Hai <lucas.tran@imt-atlantique.net>
2023-11-15 10:57:15 +0100

    RouteComponent

---
+++ components/lib/Route.vue
@@ -0,0 +1,15 @@
+<template>
+    <slot v-if="path == router.currentRoute.value.path" />
+</template>
+
+<script lang="ts" setup>
+const { path } = defineProps<{ path: string }>()
+
+const router = useRouter()
+
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/shared.scss" as *;
+</style>
+
