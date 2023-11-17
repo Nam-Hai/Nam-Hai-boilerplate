2be4379cfad19952dc1bb93b9e8c18a54458c661
Nam Hai <lucas.tran@imt-atlantique.net>
2023-11-17 15:33:06 +0100

    uId vec4 > bit oprator

--- scene/utils/WebGL.utils.ts
+++ scene/utils/WebGL.utils.ts
@@ -12,12 +12,8 @@ type uIdType = [number, number, number, number]
 export function getUId() {
     id++
     const uId: uIdType = [
-        // Math.floor(id % 256) / 256,
-        // Math.floor((id % (256 * 256)) / (256)) / 256,
-        // Math.floor((id % (256 * 256 * 256)) / (256 * 256)),
-        // Math.floor((id % (256 * 256 * 256 * 256)) / (256 * 256 * 256))
-        ((id >>  0) & 0xFF) / 0xFF,
-        ((id >>  8) & 0xFF) / 0xFF,
+        ((id >> 0) & 0xFF) / 0xFF,
+        ((id >> 8) & 0xFF) / 0xFF,
         ((id >> 16) & 0xFF) / 0xFF,
         ((id >> 24) & 0xFF) / 0xFF,
     ]
@@ -27,7 +23,7 @@ export function getUId() {
 
 import { Transform, RenderTarget, Mesh, Camera, Program, Geometry, Plane, Sphere } from 'ogl'
 import type { CanvasNode, CanvasPage } from './types'
-import type { WatchSource, WatchCallback } from 'nuxt/dist/app/compat/capi'
+import type { WatchSource, WatchCallback, ComputedGetter, DebuggerOptions } from 'nuxt/dist/app/compat/capi'
 import type { MultiWatchSources } from 'nuxt/dist/app/composables/asyncData'
 
 // fast object to get ogl class
@@ -43,17 +39,12 @@ export const O = {
 }
 
 export class EventHandler {
-    // callbacks: { event: string, cb: (e: any) => void }[]
     cbs: Map<string, Array<(e: any) => void>>
     constructor() {
-        // this.callbacks = []
         this.cbs = new Map()
     }
 
     on(event: string, cb: (e: any) => void) {
-        // if (!this.callbacks[event]) this.callbacks[event] = [];
-        // this.callbacks[event].push(cb)
-        // this.callbacks.push({ event, cb })
         if (this.cbs.has(event)) {
             const array = this.cbs.get(event)!
             array.push(cb)
@@ -64,16 +55,6 @@ export class EventHandler {
     }
 
     emit(event: string, data?: any) {
-        // const callbacks = this.callbacks[event]
-        // if (!callbacks) return
-        // for (const cb of callbacks) {
-        //     cb(data)
-        // }
-
-        // for (const a of this.callbacks) {
-        //     if (a.event == event) a.cb(a)
-        // }
-
         if (this.cbs.has(event)) {
             const callbacks = this.cbs.get(event)!
             for (const cb of callbacks) {
@@ -92,21 +73,14 @@ export class EventHandler {
     }
 }
 
-export function plugWatch(ctx: CanvasPage) {
-    return function canvasWatch(ref: MultiWatchSources | WatchSource | WatchCallback, callback: WatchCallback) {
+
+export function useCanvasReactivity(ctx: CanvasNode) {
+    function canvasWatch(ref: MultiWatchSources | WatchSource | WatchCallback, callback: WatchCallback) {
         const unWatch = watch(ref, callback)
         ctx.onDestroy(() => unWatch())
     }
-}
 
-export function plugReactivity(ctx: CanvasPage, reactivity: (...args: any[]) => () => void) {
-    return function canvasReactivity(...args: any[]) {
-        const unWatch = reactivity(...args)
-        ctx.onDestroy(() => unWatch())
+    return {
+        watch: canvasWatch
     }
-}
-
-export function canvasWatch(ctx: CanvasNode, ref: MultiWatchSources | WatchSource | WatchCallback, callback: WatchCallback) {
-    const unWatch = watch(ref, callback)
-    ctx.onDestroy(() => unWatch())
 }
