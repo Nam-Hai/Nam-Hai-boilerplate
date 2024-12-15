fda36106d06429b5c30c8654ee689213a56012ec
Nam Hai <lucas.tran@imt-atlantique.net>
2024-12-15 16:01:39 +0100

    api

--- server.bun/index.ts
+++ server.bun/index.ts
@@ -1,10 +1,60 @@
 const server = {
+    url: "http://0.0.0.0",
     port: 444
 }
 
 const path = "./levels.json"
 const file = Bun.file(path)
-const levels: GetLevelsR = await file.json()
+export type LevelsData = {
+    levels: string[]
+}
+const levels: LevelsData = await file.json()
+
+const createApi = <T extends Object, P extends ((req: Request) => Promise<T>) | (() => T)>(path: string, payload: P) => {
+    const url = new URL(path, `${server.url}:${server.port}`)
+
+    const isRequestPayload = (
+        payload: ((req: Request) => Promise<T>) | (() => T)
+    ): payload is (req: Request) => Promise<T> => {
+        return payload.length > 0;
+    };
+
+    const frontAPI = async (query: T) => {
+        const fetchResult = await fetch(url.href, {
+            method: query !== undefined ? "PUT" : "GET",
+            body: query !== undefined ? JSON.stringify(query) : undefined
+        });
+        const data: T = await fetchResult.json();
+        return data
+    }
+
+    type ServerAPIReturn<T, W> = W extends (req: Request) => Promise<T>
+        ? (req: Request) => Promise<Response>
+        : () => Response;
+
+    const serverAPI: ServerAPIReturn<T, P> = isRequestPayload(payload) ?
+        (async (req) => {
+            return Response.json(await payload(req))
+        }) as ServerAPIReturn<T, typeof payload>
+        : (() => {
+            return Response.json(payload());
+        }) as ServerAPIReturn<T, typeof payload>
+
+    return [serverAPI, frontAPI] as const
+}
+
+export const [getLevels, fetchLevels] = createApi("/levels/", () => {
+    return levels
+})
+
+export const [addlevel, fetchAddLevel] = createApi("/add/", async (req: Request) => {
+    const data: { name: string } = await req.json();
+    levels.levels.push(data.name)
+    Bun.write(path, JSON.stringify(levels))
+
+    return data;
+})
+
 
 Bun.serve({
     port: server.port,
@@ -18,13 +68,8 @@ Bun.serve({
         // For JSON data
         if (url.pathname === "/add/") {
             try {
+                return await addlevel(req)
 
-                const data = await req.json();
-                console.log(data, levels);
-                levels.levels.push(data.name)
-                Bun.write(path, JSON.stringify(levels))
-
-                return Response.json(data);
             } catch {
                 console.log('fail');
             }
@@ -35,14 +80,4 @@ Bun.serve({
         return new Response("404!");
     },
 });
-
-export type GetLevelsR = {
-    levels: string[]
-}
-function getLevels() {
-    return Response.json({
-        levels
-    });
-}
-
 console.log(`Listening on http://localhost:${server.port} ...`);
--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,10 +1,10 @@
-import type { GetLevelsR } from "~/server.bun";
+import type { LevelsData } from "~/server.bun";
 
 export default defineEventHandler(async (event) => {
   // const a = await fetch("localhost:444")
   try {
     const a = await fetch("http://0.0.0.0:444/level"); // Add the protocol
-    const data: GetLevelsR = await a.json(); // Assuming the response is JSON
+    const data: LevelsData = await a.json(); // Assuming the response is JSON
     // console.log(a.text);
     return {
       hello: 'world',
