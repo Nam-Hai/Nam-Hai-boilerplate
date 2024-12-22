5d0e3a8abafcc3369ec087ff166c8a9831c36f6c
Nam Hai <lucas.tran@imt-atlantique.net>
2024-12-22 02:52:36 +0100

    no pageFlow

--- pages/foo.vue
+++ pages/foo.vue
@@ -7,16 +7,16 @@
 <script setup lang="ts">
 import { usePageFlow } from '~/lib/waterflow/composables/usePageFlow';
 
-usePageFlow({
-    props: {
-    },
-    flowIn: (props, res) => {
-        res()
-    },
-    flowOut: (props, res) => {
-        res()
-    }
-})
+// usePageFlow({
+//     props: {
+//     },
+//     flowIn: (props, res) => {
+//         res()
+//     },
+//     flowOut: (props, res) => {
+//         res()
+//     }
+// })
 </script>
 
 <style scoped lang="scss"></style>
